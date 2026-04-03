dc0016e08d0642b200a2073785f8ba252272d639
gab-i-alves <gab.alvesnacimento@gmail.com>
2026-04-03 10:00:00 -0300

    feat: add start.sh build script and update compose for ms-conta

--- scripts/init-postgres.sql
+++ scripts/init-postgres.sql
@@ -4,3 +4,5 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bantads')\gexec
 \c bantads
 
 CREATE SCHEMA IF NOT EXISTS schema_cliente;
+CREATE SCHEMA IF NOT EXISTS schema_conta;
+CREATE SCHEMA IF NOT EXISTS schema_gerente;
