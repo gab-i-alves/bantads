3e28843288971f64e8fb0de1c10fcc5f93c4fd7c
gab-i-alves <gab.alvesnacimento@gmail.com>
2026-04-08 21:12:00 -0300

    feat: split schema_conta into schema_conta_cud and schema_conta_read

--- scripts/init-postgres.sql
+++ scripts/init-postgres.sql
@@ -4,5 +4,6 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bantads')\gexec
 \c bantads
 
 CREATE SCHEMA IF NOT EXISTS schema_cliente;
-CREATE SCHEMA IF NOT EXISTS schema_conta;
+CREATE SCHEMA IF NOT EXISTS schema_conta_cud;
+CREATE SCHEMA IF NOT EXISTS schema_conta_read;
 CREATE SCHEMA IF NOT EXISTS schema_gerente;
