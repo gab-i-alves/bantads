5a2c93f03c25f8b37488814defc7c067cb4ddc0e
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2026-04-21 13:25:41 -0300

    feat: add saga microsservice project

---
+++ ms-saga/src/main/java/br/dac/bantads/ms_saga/MsSagaApplication.java
@@ -0,0 +1,13 @@
+package br.dac.bantads.ms_saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSagaApplication.class, args);
+	}
+
+}
---
+++ ms-saga/src/test/java/br/dac/bantads/ms_saga/MsSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dac.bantads.ms_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
